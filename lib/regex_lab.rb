0751cc35d80ee875d79eb8acaec5f63ee67f85ff
Omar Gonzalez <ogonzaleznyc@gmail.com>
2021-02-04 02:54:28 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -64,7 +64,7 @@ def valid_phone_number?(phone)
 end
 
 # def valid_phone_number?(phone)
-#   if phone.match(/[0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
+#   if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
 #     true
 #   else
 #     false
